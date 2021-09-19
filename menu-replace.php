e615dce4ea6a27a6ff62683be7dc3191b24aa53e
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-09-19 16:35:06 -0400

    updates

--- menu-replace.php
+++ menu-replace.php
@@ -8,8 +8,60 @@ Fix:
 <?php
 use App\File\SplFileObjectExtended;
 
-die ("BUG: Function below only takes two arrays not four!");
- 
+
+function replace_text(string $file_name, array $replace) // <--- NEW code.
+{
+   $ifile = new SplFileObjectExtended($file_name, "r");
+
+   // Temporary outfile has the same name with extra .new extension.
+   $ofile = new  SplFileObjectExtended($file_name . '.new', "w");
+   
+   $b_menu_y = $b_menu_x = 'false';
+
+   // Note: You can't have an inner foreach identical to this one.
+   foreach ($ifile as $line) {
+       
+     $output = $line; 
+     //$line = trim($data);
+   
+     if ($line === false)
+         break;
+   
+     // Change 'charset=iso-8859-1' to 'charset=UTF-8'
+     if (strpos($line, '<meta http-equiv=') !== false) 
+   
+         $line = preg_replace('/(charset=)(?:iso|ISO)-8859-1/', '$1UTF-8', $line);
+   
+     else if (strpos($line, '<div id="menue">') !== false)
+            $b_menu_x = true;
+   
+     else if ($b_menu_x && strpos($line, "</div>") !== false) 
+            $b_menu_x = false;
+  
+     else if ($b_menu_y && strpos($line, '</div>') !== false)
+            $b_menu_y = false;   
+  
+     else if (strpos($line, '<div id="headline_container_content">') !== false)
+            $b_menu_y = true;
+
+     if ($b_menu_x === true)
+         $line = str_replace($replace['x'][0], $replace['x'][1], $line); 
+     else if ($b_menu_y === true)
+         $line = str_replace($replace['y'][0], $replace['y'][1], $line); 
+   
+     $ofile->fwrite($line . "\n");
+   }
+
+   $ifile = null; // Closes the files
+   $ofile = null;
+
+   // Move the temporary file over the original.
+   $cmd = "mv $file_name.new $file_name";
+
+   exec($cmd); 
+   
+}
+
 if ($argc != 2) {
   echo "You must enter the file name.\n";
   return;
@@ -32,7 +84,8 @@ $german_menu_x = array(
    '>Schaumburg-Lippische Genealogie',
    '>Historische Karten und Ansichten',
    '>Schaumburger Auswanderer',
-   '>Schaumburgische Bibliographie');
+   '>Schaumburgische Bibliographie',
+   '>Suche');
 
 $english_menu_x = array( '>Overview',
    '>Schaumburg in the Middle Ages',
@@ -44,7 +97,8 @@ $english_menu_x = array( '>Overview',
    '>Schaumburg-Lippe Genealogy',
    '>Historical Maps and Views',
    '>Schaumburger Emigrants',
-   '>Schaumburg Bibliography');
+   '>Schaumburg Bibliography',
+   '>Search');
 
 $german_menu_y = array(">Publikationen<",
 ">Aktuelles<",
@@ -55,63 +109,7 @@ $english_menu_y = array(
 ">Latest<",
 ">Schaumburg&#39;s History<");
 
+$replacements['x'] = array($german_menu_x, $english_menu_x);
+$replacements['y'] = array($german_menu_y, $english_menu_y);
 
-$replacments['x'] = array($german_menu_x, $english_menu_x);
-$replacments['y'] = array($german_menu_y, $english_menu_y);
-
-// function replace_text(string $file_name, array $replacments) <--- NEW code.
-
-function replace_text(string $file_name, array $german_menu, array $english_menu)
-{
-   $ifile = new SplFileObjectExtended($file_name, "r");
-
-   // Temporary outfile has the same name with extra .new extension.
-   $ofile = new  SplFileObjectExtended($file_name . '.new', "w");
-   
-   $$b_menu_y = $b_menu_x = 'false';
-
-   // Note: You can't have an inner foreach identical to this one.
-   foreach ($ifile as $line) {
-       
-     $output = $line; 
-     //$line = trim($data);
-   
-     if ($line === false)
-         break;
-   
-     // Change 'charset=iso-8859-1' to 'charset=UTF-8'
-     if (strpos($line, '<meta http-equiv=') !== false) 
-   
-         $line = preg_replace('/(charset=)(?:iso|ISO)-8859-1/', '$1UTF-8', $line);
-   
-     else if (strpos($line, '<div id="menue">') !== false)
-            $b_menu_x = true;
-   
-     else if ($b_menu_x == true && strpos($line, "</div>") !== false) 
-            $b_menu_x = false;
-  
-     else if (strpos($line, '<div id="headline_container_content">') !== false)
-            $b_menu_y = true;
-
-     else if (strpos($line, '</div>') !== false)
-            $b_menu_y = false;   
-  
-     if ($b_menu_x === true)
-         $line = str_replace($german_menu_x, $english_menu_x, $line); 
-     else if ($b_menu_y === true)
-         $line = str_replace($german_menu_y, $english_menu_y, $line); 
-   
-     $ofile->fwrite($line . "\n");
-   }
-
-   $ifile = null; // Closes the files
-   $ofile = null;
-
-   // Move the temporary file over the original.
-   $cmd = "mv $file_name.new $file_name";
-
-   exec($cmd); 
-   
-}
-
-replace_text($argv[1], $german_menu, $english_menu);
+replace_text($argv[1], $replacements);
