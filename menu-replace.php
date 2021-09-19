da789c941cd1609f77d43f3ebee9bf4b6c3edb25
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-09-19 16:04:20 -0400

    updates

--- menu-replace.php
+++ menu-replace.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL ^ E_WARNING);
 
 boot_strap();
 
-$german_menu = array(
+$german_menu_x = array(
     '>Überblick',
    '>Schaumburg im Mittelalter',
    '>Schaumburg im 16. und 17. Jahrhundert',
@@ -27,7 +27,7 @@ $german_menu = array(
    '>Schaumburger Auswanderer',
    '>Schaumburgische Bibliographie');
 
-$english_menu = array( '>Overview',
+$english_menu_x = array( '>Overview',
    '>Schaumburg in the Middle Ages',
    '>Schaumburg in the 16th and 17th Century',
    '>After 1647: Grafschaft Schaumburg, Hessish Section',
@@ -39,15 +39,23 @@ $english_menu = array( '>Overview',
    '>Schaumburger Emigrants',
    '>Schaumburg Bibliography');
 
+$german_menu_y = array(">Publikationen<",
+">Aktuelles<",
+">Geschichte Schaumburgs<");
+
+$english_menu_y = array(
+">Publications<",
+">Latest<",
+">Schaumburg&#39;s History<");
+
 function replace_text(string $file_name, array $german_menu, array $english_menu)
 {
-   
    $ifile = new SplFileObjectExtended($file_name, "r");
 
    // Temporary outfile has the same name with extra .new extension.
    $ofile = new  SplFileObjectExtended($file_name . '.new', "w");
    
-   $b_menu = 'false';
+   $$b_menu_y = $b_menu_x = 'false';
 
    // Note: You can't have an inner foreach identical to this one.
    foreach ($ifile as $line) {
@@ -64,14 +72,21 @@ function replace_text(string $file_name, array $german_menu, array $english_menu
          $line = preg_replace('/(charset=)(?:iso|ISO)-8859-1/', '$1UTF-8', $line);
    
      else if (strpos($line, '<div id="menue">') !== false)
-            $b_menu = true;
+            $b_menu_x = true;
    
-     else if ($b_menu == true && strpos($line, "</div>") !== false) 
-            $b_menu = false;
-   
-     if ($b_menu === true)
-         $line = str_replace($german_menu, $english_menu, $line); 
-   
+     else if ($b_menu_x == true && strpos($line, "</div>") !== false) 
+            $b_menu_x = false;
+  
+     else if (strpos($line, '<div id="headline_container_content">') !== false)
+            $b_menu_y = true;
+
+     else if (strpos($line, '</div>') !== false)
+            $b_menu_y = false;   
+  
+     if ($b_menu_x === true)
+         $line = str_replace($german_menu_x, $english_menu_x, $line); 
+     else if ($b_menu_y === true)
+         $line = str_replace($german_menu_y, $english_menu_y, $line); 
    
      $ofile->fwrite($line . "\n");
    }
