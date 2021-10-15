6dbeb9f0f875c8ad31b8a5293765a9b780bdc550
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2021-10-15 12:49:40 -0400

    u

--- example.php
+++ example.php
@@ -2,6 +2,7 @@
 declare(strict_types = 1);
 
 require_once "./boot-strap/boot-strap.php";
+require_once "./headers.php";
 
 error_reporting(E_ALL ^ E_WARNING);  
 
@@ -13,16 +14,15 @@ function get_paragraph($file)
 
    /*
     Finite states:
-
     b -- before paragraph
-    p -- paragraph found
+    p -- paragraph found -- not used
     i -- in paragraph
-    e -- end of paragraph
-
+    e -- end of paragraph -- not used
     */
    $state  = 'b';
+   $exit = false;
 
-   while (!$file->eof()) {
+   while ($state != 'e' && !$file->eof()) {
    
      $line = $file->current();
      
@@ -45,8 +45,9 @@ function get_paragraph($file)
   
             if (strpos($line, "</p>") === 0) { // done
 
+                $state = 'e'; 
                 $file->next();        
-                return $text; 
+                break; 
             } 
 
             $text .= (trim($line) . ' ');
@@ -56,87 +57,51 @@ function get_paragraph($file)
             break;
      }
    }
-   return preg_replace('\s\s', ' ', $text);
-   //return trim($text);
+   return preg_replace('(\s\s)', ' ', $text);
 }
 
-function add_start($file)
-{
-$header = <<<START
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-	<title></title>
-	<meta name="generator" content="LibreOffice 7.1.6.2 (Linux)"/>
-	<meta name="created" content="00:00:00"/>
-	<meta name="changed" content="2021-08-15T18:53:02.053049428"/>
-        <link rel="stylesheet" type="text/css" href="style.css" 
-</head>
-<body>
-<div id="grid-col">
-START;
+/*
+ Input: text to write followed by three columns:
 
-   $start = $header . "\n";
-   $file->fwrite($start);
-}
-
-function add_end($file)
-{
-$footer = <<<END
-</div>
-</table>
-</body>
-</html>
-END;
-
-   $end = $footer . "\n";
-   $file->fwrite($end);
-}
-
-function write_paragraphs(string $text, \SplFileObject $ofile)
+   1. The first file has two columns: German and English
+   2. 2nd file onely one column in German
+   2. 3nd file onely one column in English
+*/
+function write_paragraphs(string $text, \SplFileObject $ofile,  \SplFileObject $deFile,  \SplFileObject $enFile)
 {    
-    //$regex_colon = "/^([^:]+?)\s:\s(.*)$/";
-    $regex = "/^(.*)\s:\s(.*)$/";
+    $regex = "/^(.+)\s:\s(.*)$/U"; // Note: U is the ungreedy modifier. This ensure finding only the first  " : " sub-string,
+                                   // if there happen such sub-strings.
     
-    // debug code:
-    /* 
-    if (strpos($text, '- Nee') === 0) {
-        
-        $debug = 10;
-        ++$debug;
-    }
-    */
- 
     $rc = preg_match($regex, $text, $matches);
-    
-    //echo $matches[1] . "\n";
-    //echo $matches[2] . "\n";
-    
+  
     if ($rc === 1) {
-        
-        //--$par_class = ($matches[1][0] == '-') ? "<p class='new-speaker'>" : '<p>';
         
         if ($matches[1][0] == '-') {
              
-            $par_class = "<p class='new-speaker'>"; 
+            $par_prefix = "<p class='new-speaker'>"; 
             $matches[1] = substr($matches[1], 2);
+            
+            if (empty($matches[2])) {
+                $debug = 10;
+                
+            }
 
             if ($matches[2][0] == '-') // When the German string starts with a dash followed by a blank ("- "), the English sometimes doesn't.
                 $matches[2] = substr($matches[2], 2); 
             
-        } else {
-             $par_class = '<p>';
-        }
+        } else 
+
+           $par_prefix = '<p>';
          
+        $ofile->fwrite("{$par_prefix}$matches[1]</p>{$par_prefix}$matches[2]</p>\n");                
 
-        $ofile->fwrite("{$par_class}$matches[1]</p>{$par_class}$matches[2]</p>\n");                
-        //$ofile->fwrite("<tr>\n<td><p>$matches[1]</p></td><td><p>$matches[2]</p></td></tr>\n");
+        $deFile->fwrite("{$par_prefix}$matches[1]</p>\n");                
+
+        $enFile->fwrite("{$par_prefix}$matches[2]</p>\n");                
      
-    } else {
+    } else
 
-         throw new \ErrorException("Colon not found in paragraph:\n$text\n");
-    }
+        throw new \ErrorException("Colon not found in paragraph:\n$text\n");
 }
 
 if ($argc != 3) {
@@ -154,25 +119,32 @@ $outfile = $argv[2];
      $ifile->setFlags(SplFileObject::SKIP_EMPTY);
      
      $ofile = new \SplFileObject($outfile, "w");
+
+     $ofile->fwrite($two_cols_header);
+
+     $deFile = new \SplFileObject('de-' . $outfile, "w");
+     $enFile = new \SplFileObject('en-'.  $outfile, "w");
      
-     add_start($ofile);
+     $deFile->fwrite($one_col_header);
+     $enFile->fwrite($one_col_header);
      
      while (1) {
-     
-     
+       
          $par = get_paragraph($ifile);
      
          if (empty($par))
                break;
+             
+         write_paragraphs($par, $ofile, $deFile, $enFile);
      
-         write_paragraphs($par, $ofile);
-     
-       } 
+     } 
 
   } catch(\Exception $e)  {
      
              echo 'Caught Exception: ' . $e->getMessage() . "\n";
   }
      
-add_end($ofile);
+  $ofile->fwrite($footer . "\n");
+  $deFile->fwrite($footer . "\n");
+  $enFile->fwrite($footer . "\n");
 
---
+++ headers.php
@@ -0,0 +1,39 @@
+<?php
+
+$two_cols_header = <<<TWOCOLSSTART
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title></title>
+	<meta name="generator" content="LibreOffice 7.1.6.2 (Linux)"/>
+	<meta name="created" content="00:00:00"/>
+	<meta name="changed" content="2021-08-15T18:53:02.053049428"/>
+        <link rel="stylesheet" type="text/css" href="two-cols-style.css" 
+</head>
+<body>
+<div id="grid-col">
+TWOCOLSSTART;
+
+$one_col_header = <<<ONECOLSTART
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title></title>
+	<meta name="generator" content="LibreOffice 7.1.6.2 (Linux)"/>
+	<meta name="created" content="00:00:00"/>
+	<meta name="changed" content="2021-08-15T18:53:02.053049428"/>
+        <link rel="stylesheet" type="text/css" href="one-col-style.css" 
+</head>
+<body>
+<div id="grid-col">
+ONECOLSTART;
+
+$footer = <<<END
+</div>
+</table>
+</body>
+</html>
+END;
+
