08afec5954470d36ec35846535f498b10aa4694b
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-06-07 10:59:25 -0400

    Update utilities.php

--- utilities.php
+++ utilities.php
@@ -15,6 +15,16 @@ class NullFunctionObject  {
    }
 }
 
+class FileReader extends \SplFileObject {
+
+  public function __construct(string $fname)
+  {
+     parent::__construct($fname, "r");
+
+     $this->setFlags(\SplFileObject::READ_AHEAD | \SplFileObject::SKIP_EMPTY | \SplFileObject::DROP_NEW_LINE);
+  }
+}
+
 /*
  * Requires: 
  * 
